9f0013830e936fb34240800a28c797b7f5cfad2b
Matthew Jacques <iFtw@live.co.uk>
2022-10-04 20:20:23 +0100

    Initial commit

---
+++ TrophyIsBetter/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrophyIsBetter
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void OpenFile(string path)
+    {
+    }
+
+    private void OpenFile_Click(object sender, RoutedEventArgs e)
+    {
+      System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+      if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+      {
+        OpenFile(dialog.SelectedPath);
+      }
+    }
+  }
+}
