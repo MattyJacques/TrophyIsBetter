b66dded884c9e2a3462f52e12eb4320ec9c5b2fd
Matthew Jacques <iFtw@live.co.uk>
2022-10-08 21:41:50 +0100

    Decrypt trophy folders (#1)
    * Handle TROPCONF
    
    * Handle TROPUSR
    
    * Handle TROPTRNS
    
    * Convert TrophyParser to .NET Framework
    
    * Made some attributes public for UI

---
+++ TrophyParser/Exceptions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TrophyParser
+{
+  class InvalidFileException : Exception
+  {
+    public InvalidFileException(string fileName) : base(string.Format("Not a valid {0}.", fileName)) { }
+  }
+
+  public class AlreadyEarnedException : Exception
+  {
+    public AlreadyEarnedException(string message) : base(message) { }
+    public AlreadyEarnedException() : base("Trophy already earned.") { }
+  }
+
+  public class SyncTimeException : Exception
+  {
+    private DateTime psnSyncTime = new DateTime(0);
+    public DateTime PsnSyncTime
+    {
+      get { return psnSyncTime; }
+    }
+
+    public SyncTimeException(string message, DateTime psnSyncTime) : base(message)
+    {
+      this.psnSyncTime = psnSyncTime;
+    }
+    public SyncTimeException(DateTime psnSyncTime) : base(string.Format("The last trophy synchronized with PSN has the following date: {0:dd/MM/yyyy HH:mm:ss}. Select a date greater than this.", psnSyncTime)) { }
+  }
+
+  public class TrophyNotFound : Exception
+  {
+    public TrophyNotFound(string message) : base(message) { }
+    public TrophyNotFound() : base("Trophy ID not found.") { }
+  }
+}
---
+++ TrophyParser/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TrophyParser
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      string tempPath = Utility.File.CopyDirToTemp("C:\\Users\\Matty\\Desktop\\trophy\\PS3\\NPWR00071_00");
+      //string tempPath = Utility.File.CopyDirToTemp("C:\\Users\\Matty\\Desktop\\NPWR03907_00");
+      Utility.PfdTool.DecryptTrophyData(tempPath);
+      TROPCONF conf = new TROPCONF(tempPath);
+      TROPUSR usr = new TROPUSR(tempPath);
+      TROPTRNS trns = new TROPTRNS(tempPath);
+
+      conf.PrintState();
+      usr.PrintState();
+      trns.PrintState();
+      Utility.File.DeleteDirectory(tempPath);
+
+    }
+  }
+}
