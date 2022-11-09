bcf9d601eba39065189dd2d26633935a091248c0
Matthew Jacques <iFtw@live.co.uk>
2022-11-09 13:26:02 +0000

    Stop timestamps too far in future (#42)

--- TrophyIsBetter/ViewModels/EditTimestampViewModel.cs
+++ TrophyIsBetter/ViewModels/EditTimestampViewModel.cs
@@ -1,10 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
+using System.ComponentModel;
 using System.Windows.Input;
 
 namespace TrophyIsBetter.ViewModels
 {
-  public class EditTimestampViewModel : ObservableObject
+  public class EditTimestampViewModel : ObservableObject, IDataErrorInfo
   {
     #region Private Members
 
@@ -26,8 +27,57 @@ namespace TrophyIsBetter.ViewModels
     /// </summary>
     public ICommand OnConfirmCommand { get; set; }
 
-    public DateTime Timestamp { get => _timestamp; set => SetProperty(ref _timestamp, value); }
+    public DateTime Timestamp
+    {
+      get => _timestamp;
+      set
+      {
+        SetProperty(ref _timestamp, value);
+      }
+    }
+
+    public DateTime DateTimestamp
+    {
+      get => Timestamp;
+      set
+      {
+        Timestamp = value.AddHours(Timestamp.Hour)
+                         .AddMinutes(Timestamp.Minute)
+                         .AddSeconds(Timestamp.Second);
+      }
+    }
+
+    public string Error => throw new NotImplementedException();
+    public string this[string propertyName] => IsValid(propertyName);
 
     #endregion Public Properties
-  } // EditTimestampViewModel
+    #region Private Methods
+
+    private string IsValid(string propertyName)
+    {
+      string result = "";
+
+      switch (propertyName)
+      {
+        case "Timestamp":
+        case "DateTimestamp":
+          result = ValidateTimestamp();
+          break;
+      }
+
+      return result;
+    } // IsValid
+
+    private string ValidateTimestamp()
+    {
+      string result = "";
+
+      if (DateTime.Compare(Timestamp, DateTime.UtcNow.AddHours(14)) > 0)
+        result = "Too far in future, max 14 hours";
+
+      return result;
+    } // ValidateTimestamp
+
+      #endregion Private Methods
+    } // EditTimestampViewModel
 } // TrophyIsBetter.ViewModels
