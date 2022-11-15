db24497d38e0202577f209b5dbb9c571adb0845f
Matthew Jacques <iFtw@live.co.uk>
2022-11-15 21:55:40 +0000

    Implement game removal

---
+++ TrophyIsBetter/Extensions/Helpers/StringToImageConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace TrophyIsBetter.Extensions.Helpers
+{
+  internal class StringToImageConverter : IValueConverter
+  {
+    public object Convert(object value,
+                          Type targetType,
+                          object parameter,
+                          CultureInfo culture)
+    {
+
+      var path = (string)value;
+      // load the image, specify CacheOption so the file is not locked
+      var image = new BitmapImage();
+      image.BeginInit();
+      image.CacheOption = BitmapCacheOption.OnLoad;
+      image.UriSource = new Uri(path);
+      image.EndInit();
+
+      return image;
+
+    }
+
+    public object ConvertBack(object value,
+                              Type targetType,
+                              object parameter,
+                              CultureInfo culture)
+    {
+      throw new NotSupportedException();
+    }
+  } // StringtoImageConverter
+} // TrophyIsBetter.Extensions.Helpers
