b1e787ad69177666810470a286c5a3377dfe4d60
Matthew Jacques <iFtw@live.co.uk>
2022-11-17 21:20:19 +0000

    Reverse trophy list index

--- TrophyIsBetter/ViewModels/TrophyListViewModel.cs
+++ TrophyIsBetter/ViewModels/TrophyListViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Windows.Data;
 
 namespace TrophyIsBetter.ViewModels
