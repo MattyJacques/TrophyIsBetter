4fd41a375fba5939b5aba5862552afa7c050b9cb
Matthew Jacques <iFtw@live.co.uk>
2022-11-17 19:37:02 +0000

    Add trophy list window (#72)

--- TrophyIsBetter/Models/Trophy.cs
+++ TrophyIsBetter/Models/Trophy.cs
@@ -7,6 +7,7 @@ namespace TrophyIsBetter.Models
   {
     #region Public Properties
 
+    public string Game { get; set; }
     public int ID { get; set; }
     public string Icon { get; set; }
     public string Name { get; set; }
